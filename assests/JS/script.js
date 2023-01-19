a856521c325e8e7541ac9a064c5aea71b19cd739
Madhusriram2000 <110877875+Madhusriram2000@users.noreply.github.com>
2023-01-19 13:11:52 +0530

    first commit

---
+++ assests/JS/script.js
@@ -0,0 +1,15 @@
+fetch("./v-header.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("header").innerHTML = data;
+    });
+
+fetch("./v-footer.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("footer").innerHTML = data;
+    });
